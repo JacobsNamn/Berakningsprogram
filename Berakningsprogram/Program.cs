22bd8beda79f463a3c358c6722253375bffc8c07
Jacob <jacobgsdev@gmail.com>
2023-10-12 12:00:07 +0200

    Lade till gånger-beräkning.

--- Berakningsprogram/Program.cs
+++ Berakningsprogram/Program.cs
@@ -1,34 +1,29 @@
-﻿namespace Berakningsprogram
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿namespace Berakningsprogram {
+    internal class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hej och välkommen till beräkningsprogrammet.");
             Console.WriteLine("Skriv 'hjälp' för hjälp!");
             string command;
-            do
-            {
+            do {
                 Console.Write("Kommando: ");
                 command = Console.ReadLine();
-                if (command == "plus")
-                {
+                if (command == "plus") {
                     Console.Write("tal 1: ");
                     double X = double.Parse(Console.ReadLine());
                     Console.Write("tal 2: ");
                     double Y = double.Parse(Console.ReadLine());
                     Console.WriteLine($"{X} + {Y} = {X + Y}");
-                }
-                else if (command == "hjälp")
-                {
+                } else if (command == "gånger") {
+                    Console.Write("tal 1: ");
+                    double X = double.Parse(Console.ReadLine());
+                    Console.Write("tal 2: ");
+                    double Y = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{X} * {Y} = {X * Y}");
+                } else if (command == "hjälp") {
                     Console.WriteLine($"Tyvärr ej implementerat!");
-                }
-                else if (command == "sluta")
-                {
+                } else if (command == "sluta") {
 
-                }
-                else
-                {
+                } else {
                     Console.WriteLine($"Okänt kommando: {command}");
                 }
             } while (command != "sluta");
